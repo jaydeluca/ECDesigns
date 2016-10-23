588bf7c66206c3850f9e4f5f5c333b3bbc79478f
jaydeluca <jaydeluca4@gmail.com>
2016-10-22 22:31:55 -0400

    fix again

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,15 +1,11 @@
 <template>
     <div class="resume">
-        <div class="row">
-            <div class="col-sm-12">
-                <router-link to="/"><img src="/img/home.png" class="logo-link"></router-link>
-            </div>
+        <div class="resume-header">
+            <router-link to="/"><img src="/img/home.png" class="logo-link"></router-link>
         </div>
 
-        <div class="row">
-            <div class="col-sm-12" style="text-align: center; height: auto;">
-                <img src="/img/res_site2.png" class="img-responsive" style="margin-left: auto; margin-right: auto; padding-left: 15%; padding-right: 15%;">
-            </div>
+        <div class="resume-content">
+            <img src="/img/res_site2.png" class="img-responsive" style="margin-left: auto; margin-right: auto; padding-left: 15%; padding-right: 15%;">
         </div>
     </div>
 </template>
