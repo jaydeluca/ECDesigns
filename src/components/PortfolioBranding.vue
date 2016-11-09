6fcfaee291bc67ad633b03b00a97e901e966e138
jaydeluca <jaydeluca4@gmail.com>
2016-11-09 18:44:06 -0500

    picture update

--- src/components/PortfolioBranding.vue
+++ src/components/PortfolioBranding.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
-        <img src="/img/logo-1-veridium.jpg" class="img-responsive portfolio-item">
-        <img src="/img/logo-2-barstool-sports.jpg" class="img-responsive portfolio-item">
-        <img src="/img/logo-3-4-fingers.jpg" class="img-responsive portfolio-item">
-        <img src="/img/logo-4-personal.jpg" class="img-responsive portfolio-item">
-        <img src="/img/logo-5-silverbirch.jpg" class="img-responsive portfolio-item">
+        <img src="/img/logo-1-barstool.jpg" class="img-responsive portfolio-item">
+        <img src="/img/logo-2-veridium.jpg" class="img-responsive portfolio-item">
+        <img src="/img/logo-3-personal.jpg" class="img-responsive portfolio-item">
+        <img src="/img/logo-4-4fingers.jpg" class="img-responsive portfolio-item">
     </div>
 </template>
