52b8f9bd163a23004975cdd8441497eb96f635c4
jaydeluca <jaydeluca4@gmail.com>
2016-11-15 13:00:22 -0500

    portfolio update

--- src/components/PortfolioBranding.vue
+++ src/components/PortfolioBranding.vue
@@ -2,7 +2,10 @@
     <div>
         <img src="/img/logo-1-barstool.jpg" class="img-responsive portfolio-item">
         <img src="/img/logo-2-veridium.jpg" class="img-responsive portfolio-item">
+        <img src="/img/logo-2-veridium2.jpg" class="img-responsive portfolio-item">
         <img src="/img/logo-3-personal.jpg" class="img-responsive portfolio-item">
         <img src="/img/logo-4-4fingers.jpg" class="img-responsive portfolio-item">
+        <img src="/img/logo-5-crf.jpg" class="img-responsive portfolio-item">
+        <img src="/img/logo-5-crf2.jpg" class="img-responsive portfolio-item">
     </div>
 </template>
