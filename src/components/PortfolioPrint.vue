8ee02ee6b2108e8dc63c15023dfcb92eefdba2e5
Jay DeLuca <jaydeluca4@gmail.com>
2017-02-28 10:59:24 -0500

    update

--- src/components/PortfolioPrint.vue
+++ src/components/PortfolioPrint.vue
@@ -3,8 +3,10 @@
         <img src="/img/print-1-guinness.jpg" class="img-responsive portfolio-item">
         <img src="/img/new-belichick.jpg" class="img-responsive portfolio-item">
         <img src="/img/print-3-people.jpg" class="img-responsive portfolio-item">
-        <img src="/img/print-4-gatehouse.jpg" class="img-responsive portfolio-item">
-        <img src="/img/print-7-veridium-signs.jpg" class="img-responsive portfolio-item">
+        <img src="/img/print-4-presentation-elements.jpg" class="img-responsive portfolio-item">
+        <img src="/img/print-5-truck-day.jpg" class="img-responsive portfolio-item">
+        <img src="/img/print-6-opiates.jpg" class="img-responsive portfolio-item">
         <img src="/img/print-8-veridium-business-cards.jpg" class="img-responsive portfolio-item">
+        <img src="/img/print-9-tshirt.jpg" class="img-responsive portfolio-item">
     </div>
 </template>
